3c9ba21e47f003809eea58f0825fd2cd59f21a6e
AlexanderVieira <alexander.silva@al.infnet.edu.br>
2024-11-10 20:47:15 -0300

    chore[setup]: arquitetura base - Criação da arquitetura base

---
+++ src/AVS.OTEL.Services.API/Controllers/v1/CustomersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AVS.OTEL.Services.API.Controllers.v1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+    }
+}
